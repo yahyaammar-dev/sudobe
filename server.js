e62e6871f21654fee73905898a694c26292ceae0
lumen <yahyaammar4807@gmail.com>
2025-05-16 13:06:18 +0500

    merged with fly.io

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
