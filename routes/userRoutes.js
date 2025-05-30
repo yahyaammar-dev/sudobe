1fa04ff45ccd9288ecf91c8b5d13fe749bc2a8cb
lumen <yahyaammar4807@gmail.com>
2025-05-30 13:50:27 +0500

    updated file upoads and added apis for onbaording

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const {
+    uploadCertifications,
+    updateUserWithCertifications
+  } = require('../controllers/userController');
 
 router.post('/send-otp-via-whatsapp', userController.sendOtpViaWhatsApp)
 router.post('/verify-otp', userController.verifyOtp)
+router.post('/create-user', userController.createUser)
+router.put('/:id/certifications', uploadCertifications, updateUserWithCertifications);
+router.put('/:id/personal_id', uploadCertifications, userController.updateUserWithPersonalid);
+
 
 module.exports = router;
+
